bb472d2ccfb50b18a358846369406c8eecc60d7e
CondorDark <daividqh1995@gmail.com>
2022-03-17 14:14:30 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-main>
+        <nav>
+          <!-- <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link> -->
+        </nav>
+        <v-container>
+          <router-view/>
+        </v-container>
+      </v-main>
+    </v-app>
+  </div>
+</template>
